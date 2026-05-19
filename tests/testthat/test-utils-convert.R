a6af44df93995700463ffe7e82d0849f215a3956
Harmony Smith <harmony.aiming.smith@icloud.com>
2026-05-19 13:51:27 -0600

    Unit tests for utils-convert

---
+++ tests/testthat/test-utils-convert.R
@@ -0,0 +1,20 @@
+test_that("conversion to age-standardized rates and cases works", {
+  rr <- matrix(1:57, nrow = 19, ncol = 3)
+  pdat <- matrix(1000 + 10 * (1:57), nrow = 19, ncol = 3)
+  stdpop <- rep(0.2, 19)
+
+  expected_matrix <- cbind(c(38, 110.2, 182.4), c(2, 7, 14))
+  colnames(expected_matrix) <- c("asr", "case")
+
+  expect_equal(asry(rr, pdat, standpop = stdpop), expected_matrix)
+})
+
+test_that("conversion uses default standard population when not specified", {
+  rr <- matrix(1:57, nrow = 19, ncol = 3)
+  pdat <- matrix(1000 + 10 * (1:57), nrow = 19, ncol = 3)
+
+  expected_matrix <- cbind(c(8.477344, 27.477344, 46.477344), c(2, 7, 14))
+  colnames(expected_matrix) <- c("asr", "case")
+
+  expect_equal(asry(rr, pdat), expected_matrix)
+})
