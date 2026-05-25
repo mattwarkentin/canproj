c554ddf6ae5dda1a0ed9ba7bff665082716dcf39
Harmony Smith <harmony.aiming.smith@icloud.com>
2026-05-25 09:33:28 -0600

    Removed unused functions and standard populations

--- R/utils-convert.R
+++ R/utils-convert.R
@@ -19,18 +19,3 @@ asry <- function(rr, pdat, standpop = ca11) {
   }
   return(cbind(asr, case))
 }
-
-# asrny <- function(r0, cdat, pdat, startp, nagg, standpop = ca91) {
-#   rr <- asrpy(r0, cdat, pdat, startp = startp, nagg)
-#   c1 <- rr * pdat / 100000
-#   a1 <- rr * standpop
-#   asr <- round(apply(a1, 2, sum), 6)
-#   case <- round(apply(c1, 2, sum), 0)
-#   for (i in 1:length(asr)) {
-#     if (asr[i] < 0) asr[i] <- 0
-#   }
-#   for (i in 1:length(case)) {
-#     if (case[i] < 0) case[i] <- 0
-#   }
-#   return(cbind(asr, case))
-# }
