f33351aee890bc3665f0b31ad5a5f94359f65859
Harmony Smith <harmony.aiming.smith@icloud.com>
2026-05-26 10:54:07 -0600

    Better test strategy for utils data aggregation and standardize

--- tests/testthat/test-utils-data-aggregation.R
+++ tests/testthat/test-utils-data-aggregation.R
@@ -2,69 +2,66 @@ test_that("aggregation by 1 returns all years without aggregation", {
   cdat <- matrix(1:19, nrow = 19, ncol = 15)
   pdat <- matrix(1:19, nrow = 19, ncol = 20)
 
-  agg <- datagg(cdat, pdat, 1)
-  expect_equal(length(agg), 2)
-
-  expected_cases <- data.frame(
+  cases <- data.frame(
     matrix(1:19, nrow = 19, ncol = 15),
     row.names = c(1:19)
   )
-  colnames(expected_cases) <- 1:15
-  expect_equal(agg$cases, expected_cases)
+  colnames(cases) <- 1:15
 
-  expected_pyr <- data.frame(
+  pyr <- data.frame(
     matrix(1:19, nrow = 19, ncol = 20),
     row.names = c(1:19)
   )
-  colnames(expected_pyr) <- 1:20
-  expect_equal(agg$pyr, expected_pyr)
+  colnames(pyr) <- 1:20
+
+  agg <- list(cases = cases, pyr = pyr)
+
+  expect_equal(datagg(cdat, pdat, 1), agg)
 })
 
 test_that("aggregation by a factor of nyears aggregates all years", {
   cdat <- matrix(1:19, nrow = 19, ncol = 15)
   pdat <- matrix(1:19, nrow = 19, ncol = 20)
 
-  agg <- datagg(cdat, pdat, 5)
-  expect_equal(length(agg), 2)
-
-  expected_cases <- data.frame(
+  cases <- data.frame(
     matrix(5 * (1:19), nrow = 19, ncol = 3),
     row.names = c(1:19)
   )
-  colnames(expected_cases) <- 1:3
-  expect_equal(agg$cases, expected_cases)
+  colnames(cases) <- 1:3
 
-  expected_pyr <- data.frame(
+  pyr <- data.frame(
     matrix(5 * (1:19), nrow = 19, ncol = 4),
     row.names = c(1:19)
   )
-  colnames(expected_pyr) <- 1:4
-  expect_equal(agg$pyr, expected_pyr)
+  colnames(pyr) <- 1:4
+
+  agg <- list(cases = cases, pyr = pyr)
+
+  expect_equal(datagg(cdat, pdat, 5), agg)
 })
 
 test_that("aggregation by a nonfactor removes extra years at beginning", {
   cdat <- matrix(1:95, nrow = 19, ncol = 5)
   pdat <- matrix(1:133, nrow = 19, ncol = 7)
 
-  agg <- datagg(cdat, pdat, 2)
-  expect_equal(length(agg), 2)
-
-  expected_cases <- data.frame(
+  cases <- data.frame(
     a = 57 + 2 * (1:19),
     b = 133 + 2 * (1:19),
     row.names = c(1:19)
   )
-  colnames(expected_cases) <- 1:2
-  expect_equal(agg$cases, expected_cases)
+  colnames(cases) <- 1:2
 
-  expected_pyr <- data.frame(
+  pyr <- data.frame(
     a = 57 + 2 * (1:19),
     b = 133 + 2 * (1:19),
     c = 209 + 2 * (1:19),
     row.names = c(1:19)
   )
-  colnames(expected_pyr) <- 1:3
-  expect_equal(agg$pyr, expected_pyr)
+  colnames(pyr) <- 1:3
+
+  agg <- list(cases = cases, pyr = pyr)
+
+  expect_equal(datagg(cdat, pdat, 2), agg)
 })
 
 test_that("population input must include projection years", {
--- tests/testthat/test-utils-standardize.R
+++ tests/testthat/test-utils-standardize.R
@@ -3,12 +3,17 @@ test_that("obasr calculates annual age-standardized rates by cases", {
   pdat <- matrix(100, nrow = 19, ncol = 20)
   stdpop <- c(rep(0.02, 5), rep(0.04, 5), rep(0.06, 5), rep(0.1, 4))
 
-  out <- obasr(cdat, pdat, stdpop)
+  out <- matrix(
+    c(
+      c(12800, 31800, 50800, 69800, 88800),
+      c(190, 551, 912, 1273, 1634)
+    ),
+    nrow = 5,
+    ncol = 2
+  )
+  colnames(out) <- c("asr", "case")
 
-  expect_equal(nrow(out), 5)
-  expect_equal(ncol(out), 2)
-  expect_equal(out[, 1], c(12800, 31800, 50800, 69800, 88800))
-  expect_equal(out[, 2], c(190, 551, 912, 1273, 1634))
+  expect_equal(obasr(cdat, pdat, stdpop), out)
 })
 
 test_that("obasr calculates annual age-standardized rates by population", {
@@ -16,32 +21,34 @@ test_that("obasr calculates annual age-standardized rates by population", {
   pdat <- matrix(100 + 2 * (1:95), nrow = 19, ncol = 20)
   stdpop <- c(rep(0.02, 5), rep(0.04, 5), rep(0.06, 5), rep(0.1, 4))
 
-  out <- obasr(cdat, pdat, stdpop)
-
-  expect_equal(nrow(out), 5)
-  expect_equal(ncol(out), 2)
-  expect_equal(
-    out[, 1],
-    c(4007.581, 3068.095, 2486.432, 2090.523, 1803.528),
-    tolerance = 0.001
+  out <- matrix(
+    c(
+      c(4007.581, 3068.095, 2486.432, 2090.523, 1803.528),
+      rep(95, 5)
+    ),
+    nrow = 5,
+    ncol = 2
   )
-  expect_equal(out[, 2], rep(95, 5))
+  colnames(out) <- c("asr", "case")
+
+  expect_equal(obasr(cdat, pdat, stdpop), out, tolerance = 0.001)
 })
 
 test_that("obasr uses Canada 2011 standard population for default", {
   cdat <- matrix(1:95, nrow = 19, ncol = 5)
   pdat <- matrix(100, nrow = 19, ncol = 20)
 
-  out <- obasr(cdat, pdat)
-
-  expect_equal(nrow(out), 5)
-  expect_equal(ncol(out), 2)
-  expect_equal(
-    out[, 1],
-    c(8477.344, 27477.344, 46477.344, 65477.344, 84477.344),
-    tolerance = 0.001
+  out <- matrix(
+    c(
+      c(8477.344, 27477.344, 46477.344, 65477.344, 84477.344),
+      c(190, 551, 912, 1273, 1634)
+    ),
+    nrow = 5,
+    ncol = 2
   )
-  expect_equal(out[, 2], c(190, 551, 912, 1273, 1634))
+  colnames(out) <- c("asr", "case")
+
+  expect_equal(obasr(cdat, pdat), out, tolerance = 0.001)
 })
 
 test_that("asrsd calculates age-standardized rates and standard error", {
@@ -49,40 +56,34 @@ test_that("asrsd calculates age-standardized rates and standard error", {
   pdat <- matrix(200 + 2 * (1:95), nrow = 19, ncol = 20)
   stdpop <- c(rep(0.02, 5), rep(0.04, 5), rep(0.06, 5), rep(0.1, 4))
 
-  out <- asrsd(cdat, pdat, standpop = stdpop)
+  out <- matrix(
+    c(
+      c(5584.634, 12008.157, 16806.539, 20527.776, 23498.211),
+      c(432.9002, 565.0387, 619.3358, 642.7181, 650.9717)
+    ),
+    nrow = 5,
+    ncol = 2
+  )
+  colnames(out) <- c("asr", "asd")
 
-  expect_equal(nrow(out), 5)
-  expect_equal(ncol(out), 2)
-  expect_equal(
-    out[, 1],
-    c(5584.634, 12008.157, 16806.539, 20527.776, 23498.211),
-    tolerance = 0.001
-  )
-  expect_equal(
-    out[, 2],
-    c(432.9002, 565.0387, 619.3358, 642.7181, 650.9717),
-    tolerance = 0.0001
-  )
+  expect_equal(asrsd(cdat, pdat, standpop = stdpop), out, tolerance = 0.001)
 })
 
 test_that("asrsd uses Canada 2011 standard population by default", {
   cdat <- matrix(1:95, nrow = 19, ncol = 5)
   pdat <- matrix(200 + 2 * (1:95), nrow = 19, ncol = 20)
 
-  out <- asrsd(cdat, pdat)
+  out <- matrix(
+    c(
+      c(3828.828, 10728.919, 15833.109, 19762.243, 22880.420),
+      c(321.1489, 506.4917, 575.8266, 605.9329, 617.8074)
+    ),
+    nrow = 5,
+    ncol = 2
+  )
+  colnames(out) <- c("asr", "asd")
 
-  expect_equal(nrow(out), 5)
-  expect_equal(ncol(out), 2)
-  expect_equal(
-    out[, 1],
-    c(3828.828, 10728.919, 15833.109, 19762.243, 22880.420),
-    tolerance = 0.001
-  )
-  expect_equal(
-    out[, 2],
-    c(321.1489, 506.4917, 575.8266, 605.9329, 617.8074),
-    tolerance = 0.0001
-  )
+  expect_equal(asrsd(cdat, pdat), out, tolerance = 0.001)
 })
 
 test_that("chper calculates percentage change", {
@@ -92,23 +93,18 @@ test_that("chper calculates percentage change", {
   byear <- 2001
   cyear <- 2004
 
-  out <- chper(aspr, pdat, byear, cyear, starty = starty)
+  out <- data.frame(
+    ref.case = 0.00238298,
+    comp.case = 0.00703988,
+    overall = 195.42337745,
+    risk = 123.61664806,
+    p.growth = 72.15189873,
+    p.aging = -0.34516934
+  )
 
   expect_equal(
-    colnames(out),
-    c("ref.case", "comp.case", "overall", "risk", "p.growth", "p.aging")
-  )
-  expect_equal(nrow(out), 1)
-  expect_equal(
-    unname(unlist(out[1, ])),
-    c(
-      0.00238298,
-      0.00703988,
-      195.42337745,
-      123.61664806,
-      72.15189873,
-      -0.34516934
-    ),
+    chper(aspr, pdat, byear, cyear, starty = starty),
+    out,
     tolerance = 0.0000001
   )
 })
@@ -122,23 +118,18 @@ test_that("chper works with NULL start year", {
   byear <- 2001
   cyear <- 2004
 
-  out <- chper(aspr, pdat, byear, cyear)
+  out <- data.frame(
+    ref.case = 0.00238298,
+    comp.case = 0.00703988,
+    overall = 195.42337745,
+    risk = 123.61664806,
+    p.growth = 72.15189873,
+    p.aging = -0.34516934
+  )
 
   expect_equal(
-    colnames(out),
-    c("ref.case", "comp.case", "overall", "risk", "p.growth", "p.aging")
-  )
-  expect_equal(nrow(out), 1)
-  expect_equal(
-    unname(unlist(out[1, ])),
-    c(
-      0.00238298,
-      0.00703988,
-      195.42337745,
-      123.61664806,
-      72.15189873,
-      -0.34516934
-    ),
+    chper(aspr, pdat, byear, cyear),
+    out,
     tolerance = 0.0000001
   )
 })
