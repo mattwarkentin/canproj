92374c1bacd6c14d20feac9436188fe9db3840a4
Harmony Smith <harmony.aiming.smith@icloud.com>
2026-08-18 14:29:21 -0600

    Removed unused datasets

--- data-raw/DATASET.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-cdat <- matrix(round(rnorm(19 * 15, 1000, 100)), nrow = 19)
-
-pdat <- matrix(round(rnorm(19 * (15 + 15), 1e5, 5000)), nrow = 19)
-
-usethis::use_data(cdat, pdat, overwrite = TRUE)
